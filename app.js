f03b95114393135403e9d1a99dcda19b87efe661
weilok2021 <weilok629@gmail.com>
2025-09-20 17:26:43 +0800

    Implementing a getGameStatus method in GameBoard
    Added a winning condition check: IF 3 consecutive elements in a row is equal
    This condition check is tested on few test cases

--- app.js
+++ app.js
@@ -6,8 +6,36 @@ const GameBoard = (function () {
     // INITIALIZE the game board 
     let gameBoard = [];
 
+    function isValidIndex(row, col) {
+        const isValid = (row >= 0 && row < gameBoard.length) && (col >= 0 && col < gameBoard.length)
+        return isValid;
+    }
+
+    function isEmptySpace(row, col) {
+        const isEmpty = gameBoard[row][col] === '';
+        return isEmpty;
+    }
     // A terminal function to tell if the game is ended on this board
     // RETURN which side has won, or tie.
+    function getGameStatus() {
+        for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard.length; j++) {
+                // win condition as a horizontal line (row)
+                if (!isEmptySpace(i, j)) {
+                    if (isValidIndex(i, j + 1) && isValidIndex(i, j + 2)) {
+                        console.log(i, j);
+                        console.log(i, j + 1);
+                        console.log(i, j + 2);
+                        console.log(gameBoard[i][j] === gameBoard[i][j + 1] === gameBoard[i][j + 2]);
+                        if (gameBoard[i][j] === gameBoard[i][j + 1] && gameBoard[i][j + 1] === gameBoard[i][j + 2]) {
+                            console.log(`player has won!`);
+                            return gameBoard[i][j]; // return the won side
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     function initializeBoardState(row, col) {
         gameBoard = [];
@@ -26,10 +54,9 @@ const GameBoard = (function () {
     function move(row, col, side) {
         function isValidMove(row, col) {
             const isNum = typeof row === "number" && typeof col === "number";
-            const validIndex = (row >= 0 && row < gameBoard.length) && (col >= 0 && col < gameBoard.length)
-            if (isNum && validIndex) {
-                const isEmpty = gameBoard[row][col] === '';
-                if (isEmpty) {
+            // const isValidIndex = (row >= 0 && row < gameBoard.length) && (col >= 0 && col < gameBoard.length)
+            if (isNum && isValidIndex(row, col)) {
+                if (isEmptySpace(row, col)) {
                     console.log("This is a valid move.");
                     return true;
                 }
@@ -43,8 +70,8 @@ const GameBoard = (function () {
         }
     }
 
-    return { move, initializeBoardState, displayBoardState };
-}) ()
+    return { move, initializeBoardState, displayBoardState, getGameStatus };
+})()
 
 const createPlayer = function () {
     let score = 0;
